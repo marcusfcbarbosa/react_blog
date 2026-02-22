90414da798705b3caaa368cac42a46f808b1e6cf
Barbosa, Marcus <Marcus.Barbosa+ual@united.com>
2026-02-22 18:58:39 -0300

    setup the post service saving in memory, setupo the json serialization

---
+++ post/index.js
@@ -0,0 +1,29 @@
+//create express setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json()); //return in jason format
+const { randomBytes } = require('crypto');
+//store for now in memory
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    //associate a id with a post created 
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id]={
+        id,title
+    };  
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log('Post service Listenning 4000 port.');
+});
