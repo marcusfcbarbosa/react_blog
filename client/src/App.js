6d1619cb5f7d9ab1e7a8a1f9746e75a38a981ed2
Barbosa, Marcus <Marcus.Barbosa+ual@united.com>
2026-02-23 21:36:26 -0300

    className='container' to expand input

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PostCreate from './PostCreate';
 export default function App() {
-  return <div>
+  return <div className='container'>
 
     <h1>Create Post</h1>
     <PostCreate />
